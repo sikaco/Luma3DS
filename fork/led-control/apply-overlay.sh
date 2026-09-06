2db709b60c93b75366d09f5466597600f6ef2f03
WarmBear <sikaco0@gmail.com>
2026-09-06 17:45:33 +0800

    led: use asserted integration script

--- fork/led-control/apply-overlay.sh
+++ fork/led-control/apply-overlay.sh
@@ -5,5 +5,4 @@ cd "$repo"
 cp fork/led-control/led_control.c sysmodules/rosalina/source/led_control.c
 cp fork/led-control/led_control_part*.inc sysmodules/rosalina/source/
 cp fork/led-control/led_control.h sysmodules/rosalina/include/led_control.h
-git apply --check fork/led-control/integration.patch
-git apply fork/led-control/integration.patch
+python3 fork/led-control/integrate.py
